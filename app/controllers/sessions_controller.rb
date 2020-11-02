98d640bc427026844deedd9f5f2e38b3e8314072
gabrielHilarion <gabrielhhilarion@gmail.com>
2020-11-02 13:36:16 +0100

    create session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        user = User.find_by_email(params[:email])
+
+        if user 
+            session[:user_id] = user.id 
+            redirect_to users_path, notice: 'Logged in'
+        else
+            flash.now[:alert] = 'email does not exist'
+            render :new 
+        end
+    end
+
+    def destroy 
+        session[:user_id] = nil 
+        redirect_to root_path, notice: 'You have been logged out'
+    end
+
+end
